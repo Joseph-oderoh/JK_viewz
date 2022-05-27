5ef6aed7b333e3720e357d88c5ca59b646f0722e
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-27 16:20:14 +0300

    rf:set secret key hidden

--- gallery/settings.py
+++ gallery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-70#^y(1cnbsyuha#rfb7qm7%-n$)wdpugwp!4wep9l-l#@29x$'
+SECRET_KEY =('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
