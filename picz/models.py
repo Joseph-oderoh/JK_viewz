ed37511025e513385b723a761308f25bd0e2aa91
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-28 09:30:55 +0300

    add models to database

--- picz/models.py
+++ picz/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+    '''
+    model to handle images
+    '''
+    image_link = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE, default=1)
+    location = models.ForeignKey('Locations', on_delete=models.CASCADE, default=1)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Categories(models.Model):
+    '''
+    model to handle categories
+    '''
+    name = models.CharField(max_length=30)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Locations(models.Model):
+    '''
+    model to handle locations
+    '''
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)    
