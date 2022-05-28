b2c0b79b50023b84a9ad4f0d344b95410ef2d3b7
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-28 13:32:21 +0300

    add veiw function to app

--- picz/urls.py
+++ picz/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.homepage, name='landing'),
+    path('gallery/', views.gallery, name='gallery'),
 ]
+
--- picz/views.py
+++ picz/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Images, Locations
 # Create your views here.de
 
 
 def homepage(request):
     return render(request, 'welcome.html')
+
+def gallery(request):
+    pictures = Images.get_all()
+    locations = Locations.get_all()
+    return render(request, 'gallery.html', {'pictures': pictures, 'locations':locations})
+
+
+def location(request,locale):
+    images = Images.filter_by_location(locale)
+    return render(request, 'location.html', {'results':images})
+
 
