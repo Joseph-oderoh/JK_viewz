40b5fb3e70ee10f590ae97c653864cb67bf7ea45
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-29 09:46:15 +0300

    add the location template

--- picz/urls.py
+++ picz/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.homepage, name='landing'),
     path('gallery/', views.gallery, name='gallery'),
     path('search/', views.search, name='search'),
+    re_path('^location/(?P<locale>\w+)/$', views.location, name='location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
